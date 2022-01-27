2d3eacbaef026074eecc76649685d6b47e2023ff
pdor <pdor92@hotmail.it>
2022-01-27 01:48:31 +0100

    Removed some code duplication.

--- script.js
+++ script.js
@@ -4,28 +4,6 @@ function makeEtchEventListener(square) {
     };
 }
 
-// create the square divs in the container
-let container = document.querySelector("#grid-container");
-
-// create rows
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-
-    //create squares
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.style.height = (800 / 16).toString() + "px";
-        square.style.width = (800 / 16).toString() + "px";
-        square.addEventListener('mouseover', () => square.classList.add("painted"));
-        row.appendChild(square);
-    }
-
-    container.appendChild(row);
-}
-
-
 // Reset and resize grid
 function reset(newSize) {
     let container = document.querySelector("#grid-container");
@@ -58,3 +36,5 @@ let resetButton = document.querySelector("#reset");
 resetButton.addEventListener('click', function() {
     reset(prompt("New Size (max 100): "));
 });
+
+reset(16);
