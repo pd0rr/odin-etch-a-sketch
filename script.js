dd3c51c0622da64ddf29951bcd15e3d6140c1ba9
pdor <pdor92@hotmail.it>
2022-01-27 01:02:32 +0100

    Add a square to the container.

---
+++ script.js
@@ -0,0 +1,7 @@
+// create the square divs in the container
+let container = document.querySelector("#grid-container");
+
+let square = document.createElement("div");
+square.classList.add("square");
+
+container.appendChild(square);
