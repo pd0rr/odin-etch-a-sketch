e9ade67b027e14504377e4ecaa4203dbeb356515
pdor <pdor92@hotmail.it>
2022-01-27 01:07:51 +0100

    Create a grid of 16 squares.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // create the square divs in the container
 let container = document.querySelector("#grid-container");
 
-let square = document.createElement("div");
-square.classList.add("square");
+// create rows
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
 
-container.appendChild(square);
+    //create squares
+    for (let j = 0; j < 16; j++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        row.appendChild(square);
+    }
+
+    container.appendChild(row);
+}
